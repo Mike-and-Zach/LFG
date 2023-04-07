d6ce5613aad6ed02850c0b6e65665f97ae4a27c2
Zachary Oliver <zachary.oliver@yahoo.com>
2023-04-07 00:02:28 -0700

    api calls for edit and delte posts

--- api/posts.js
+++ api/posts.js
@@ -1,28 +1,48 @@
 const express = require("express");
 const router = express.Router();
 
-
-const { getAllPosts, createPost, editPost, deletePost} = require("../db/models/posts");
+const {
+  getAllPosts,
+  createPost,
+} = require("../db/models/posts");
 
 router.get("/", async (req, res, next) => {
-    try {
-        const posts = await getAllPosts();
-        res.send(posts)
-    } catch (err) {
-        next(err)
-    }
-
-})
+  try {
+    const posts = await getAllPosts();
+    res.send(posts);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post("/", async (req, res, next) => {
+  try {
+    const { gameTitle, description } = req.body;
+    const createdPost = await createPost({ gameTitle, description });
+    res.send(createdPost);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+router.delete("/:postId", async (req, res, next) => {
     try {
-        const { gameTitle,description } = req.body;
-        const createdPost = await createPost({gameTitle, description});
-        res.send(createdPost)
-    } catch ({name, message}) {
-        next({name, message})
+      const postId = req.params.postId;
+      const deletedPost = await deletePost(postId);
+      res.send(deletedPost);
+    } catch ({ name, message }) {
+      next({ name, message });
     }
-})
+  });
+  
+  router.patch("/:postId", async (req, res, next) => {
+    try {
+      const postId = req.params.postId;
+      const updatedPost = await editPost({ id: postId, ...req.body });
+      res.send(updatedPost);
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
 
-
-module.exports = router
+module.exports = router;
