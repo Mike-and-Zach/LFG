80fc5de60b6f9ce3d51db85793a2217f623c31a5
Michael  McEwing <107093761+Duke650@users.noreply.github.com>
2023-04-06 17:18:46 -0700

    added dropdown for creating posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,7 +6,6 @@ const Posts = () => {
     const [showMakePost, setShowMakePost] = useState(false)
     const [gameTitle, setGameTitle] = useState("");
     const [description, setDescription] = useState("");
-
     const fetchPosts = async () => {
         try {
             const data = await callApi({
@@ -40,8 +39,19 @@ const Posts = () => {
         return (
             <div>
                 <form>
-                    <label htmlFor="gameTitle">Game Title: </label> <br />
-                    <input type="text" id="gameTitle" onChange={e => {setGameTitle(e.target.value)}}/> <br /> <br />
+                    <label htmlFor="gameTitle">Choose your game: </label> <br />
+                    <select name="games" id="gameTitle" onChange={e => {setGameTitle(e.target.value)}}>
+                        <option disabled selected value>-- Choose a game --</option>
+                        <option value="COD">COD</option>
+                        <option value="Overwatch">Overwatch</option>
+                        <option value="DayZ">DayZ</option>
+                        <option value="Counter-Strike 2">Counter-Strike 2</option>
+                        <option value="Destiny">Destiny</option>
+                        <option value="Valorant">Valorant</option>
+                        <option value="RB6">RB6</option>
+                        <option value="GTA6">GTA6</option>
+                        <option value="League of Legends">League of Legends</option>
+                    </select> <br />
                     <label htmlFor="description">Description: </label> <br />
                     <input type="text" id="description" onChange={e => {setDescription(e.target.value)}}/> <br /> <br />
                     <input type="submit" value="Create Post!" onClick={() => handleMakePost()}/>
